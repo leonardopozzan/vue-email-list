b6700e58786807b925c58c231739dbb4b0497382
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2022-11-08 21:59:19 +0100

    es finito

--- js/script.js
+++ js/script.js
@@ -3,10 +3,26 @@ const {createApp} = Vue;
 const app = createApp({
     data(){
         return{
-            mailArray: []
+            mails: [],
+            numb: 10
         }
     },
     methods:{
-
+        generateMail(){
+            if(this.numb > 0){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((element)=>{
+                this.mails.push(element.data.response);
+                })
+            }
+            this.$nextTick(()=>this.$refs.adress[this.$refs.adress.length-1].scrollIntoView({behavior: "smooth"}))
+        },
+        generateMails(){
+            this.mails=[];
+            for(let i =0; i<this.numb;i++){
+                this.generateMail();
+            }
+            this.$nextTick(()=>this.$refs.adress[this.$refs.adress.length-1].scrollIntoView({behavior: "smooth"}))
+        }
     }
 })
+app.mount('#app');
