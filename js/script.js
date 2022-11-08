e21bcf301fc30b3d633d1cac69dae1f16a2ccef0
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2022-11-08 23:46:41 +0100

    scroll fino all'ultima mail

--- js/script.js
+++ js/script.js
@@ -4,24 +4,51 @@ const app = createApp({
     data(){
         return{
             mails: [],
-            numb: 10
+            numb: 40
         }
     },
     methods:{
         generateMail(){
-            if(this.numb > 0){
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((element)=>{
-                this.mails.push(element.data.response);
-                })
-            }
-            this.$nextTick(()=>this.$refs.adress[this.$refs.adress.length-1].scrollIntoView({behavior: "smooth"}))
+            
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((element)=>{
+                this.mails.push(element.data.response)
+                console.log(this.mails.length);
+                if(this.numb == this.mails.length){
+                    this.$nextTick(()=>{
+                        this.$refs.adress[this.$refs.adress.length-1].scrollIntoView({behavior: "smooth"})
+                        console.log(this.$refs.adress[this.$refs.adress.length-1])
+                    })
+                }
+            });
+            
+            // this.$nextTick(()=>{
+            //     this.$refs.adress[this.$refs.adress.length-1].scrollIntoView({behavior: "smooth"})
+            //     console.log(this.$refs.adress[this.$refs.adress.length-1])
+            // })
         },
         generateMails(){
             this.mails=[];
             for(let i =0; i<this.numb;i++){
                 this.generateMail();
             }
-            this.$nextTick(()=>this.$refs.adress[this.$refs.adress.length-1].scrollIntoView({behavior: "smooth"}))
+            
+            // scroll.then(()=> scroll.scrollIntoView({behavior: "smooth"}))
+            // this.$nextTick(()=>{
+            //     this.$refs.adress[this.$refs.adress.length-1].scrollIntoView({behavior: "smooth"})
+            // })
+        },
+        addMail(){
+            this.numb++;
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((element)=>{
+                this.mails.push(element.data.response)
+                console.log(this.mails.length);
+                if(this.numb == this.mails.length){
+                        this.$nextTick(()=>{
+                        this.$refs.adress[this.$refs.adress.length-1].scrollIntoView({behavior: "smooth"})
+                        console.log(this.$refs.adress[this.$refs.adress.length-1])
+                    })
+                }
+            });
         }
     }
 })
